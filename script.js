ee824b4ef1110044c66cb4c352ce938743a951a5
Gabriel Bravo Antonio Silva <gabrielbravo4321@gmail.com>
2025-08-19 11:43:49 -0300

    first commit

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ const SUPABASE_URL = 'https://tkttoczozozdpxtgqqgf.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdHRvY3pvem96ZHB4dGdxcWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNzg2NzYsImV4cCI6MjA3MDg1NDY3Nn0.8F4b0yuynIatHp32F-LeMNT539yHehcV-h_zZ8iaWGw';
 // Alteração: use outro nome para o cliente Supabase para evitar conflito
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const { data, error } = await supabaseClient
+    .rpc('fazer_login', {
+        p_usuario: usuario,
+        p_senha: senha
+    });
 
 async function recarregarSchema() {
     try {
